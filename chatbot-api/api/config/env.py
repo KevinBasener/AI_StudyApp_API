3ec1c4227acf52ea28d1bd89c41dcff4ab05245a
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2025-01-28 14:37:30 +0100

    Use groq for ollama

--- chatbot-api/api/config/env.py
+++ chatbot-api/api/config/env.py
@@ -9,3 +9,6 @@ OLLAMA_API_BASE = os.getenv(
 FRONTEND_URL = os.getenv(
     "FRONTEND_URL", "http://localhost:3000"
 )  # Read from environment variable
+GROQ_API_BASE = os.getenv("GROQ_API_BASE", "https://api.groq.com/openai/v1")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY",
+                         "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")  # Read from environment variable
